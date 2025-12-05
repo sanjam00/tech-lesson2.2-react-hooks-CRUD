ddb613160bb21a4b5f09146d16120bdffbd3e8ed
Sanaya James <sanaya.j.james@gmail.com>
2025-12-04 20:56:47 -0700

    add create item functionality to the new item form, and render items on page load

--- src/components/ShoppingList.jsx
+++ src/components/ShoppingList.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ItemForm from "./ItemForm";
 import Filter from "./Filter";
 import Item from "./Item";
@@ -6,6 +6,26 @@ import Item from "./Item";
 function ShoppingList() {
   const [selectedCategory, setSelectedCategory] = useState("All");
   const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/items")
+      .then(response => {
+        if (response.ok) {
+          return response.json()
+        } else {
+          console.error("fetch request failed")
+        }
+      })
+      .then(items => setItems(items))
+      .catch(error =>
+        console.log("Failed to fetch:", error)
+      )
+  }, [])
+
+  function handleAddItem(newItem) {
+    console.log("In shoppinglist:", newItem)
+    setItems([...items, newItem])
+  }
 
   function handleCategoryChange(category) {
     setSelectedCategory(category);
@@ -19,7 +39,7 @@ function ShoppingList() {
 
   return (
     <div className="ShoppingList">
-      <ItemForm />
+      <ItemForm onAddItem={handleAddItem} />
       <Filter
         category={selectedCategory}
         onCategoryChange={handleCategoryChange}
